6866ad4b8e781b27c13ec624d05af4043306cb90
novaprospectCAU <novaprospect110a@gmail.com>
2024-01-19 13:51:45 +0900

    lamp추가 및 일부 수정하였습니다. 실험 코드들을 롤백하여 모두 삭제하거나 복원했습니다

--- srcs/display.js
+++ srcs/display.js
@@ -10,6 +10,9 @@ import {
 
 const PI = Math.PI.toFixed(8);
 
+const lampImg = new Image();
+lampImg.src = "./../assets/lamp.png";
+
 export function colorMix(number, RGB) {
   if (number === C2RAYS / 2) return;
   if (number < C2RAYS / 2) {
@@ -18,4 +21,14 @@ export function colorMix(number, RGB) {
     RGB[2] += 2 * (C2RAYS / 2 - number);
   } else {
   }
+}
+
+export async function lampDraw(animationNumber) {
+  //12 animation tick per one animation cycle
+
+  const angle1 = PI / 60;
+  const angle2 = PI / 30;
+  const angle3 = PI / 20;
+
+  gameplay.drawImage(lampImg, (C2WIDTH * 1.3) / 4 + 10, 0, 200, 200);
 }
--- srcs/main.js
+++ srcs/main.js
@@ -1,6 +1,7 @@
 import { minimapInit, minimapDraw, leftCanvas, minimap } from "./minimap.js";
 import { playerInit, playerDraw, laserDraw, lightDraw } from "./player.js";
 import { rightCanvas, gameplay, gameplayBackgroundDraw } from "./raycast.js";
+import { lampDraw } from "./display.js";
 
 const PI = Math.PI.toFixed(8);
 const ANIMATION_TICK = 1 / 60;
@@ -8,6 +9,7 @@ const ANIMATION_TICK = 1 / 60;
 minimapInit();
 playerInit();
 
+let animationNumber = 0;
 function gameLoop() {
   minimap.clearRect(0, 0, leftCanvas.width, leftCanvas.height);
   gameplay.clearRect(0, 0, rightCanvas.width, rightCanvas.height);
@@ -17,6 +19,9 @@ function gameLoop() {
   playerDraw();
   lightDraw();
   laserDraw();
+  lampDraw(animationNumber);
+  // animationNumber++;
+  // animationNumber = animationNumber > 120 ? 0 : animationNumber;
 
   setTimeout(gameLoop, ANIMATION_TICK);
 }
